c3318f0c37549f0049ef7175c2879dd48f2a659a
Matt Moretti <werebus@gmail.com>
2016-04-09 21:51:05 -0400

    nodm builds now (required some toolchain changes)

--- package/nodm/nodm.mk
+++ package/nodm/nodm.mk
@@ -1,13 +1,17 @@
 ################################################################################
 #
-# openbox
+# nodm
 #
 ################################################################################
 
-NODM_VERSION = debian/0.12-1
-NODM_SITE = $(call github,spanezz,nodm,$(NODM_VERSION))
+# Would be better to use a tag, unfortunately nodm tags releases with
+# '/'s in them and I don't know how to do that. This is 'debian/0.12-1'
+NODM_VERSION = 22eba9bb71fc6951ddb659c1704a71870705d923
+NODM_SOURCE = $(NODM_VERSION).tar.gz
+NODM_SITE = $(call github, spanezz, nodm, '')
 NODM_LICENSE = GPLv2+
 NODM_LICENSE_FILES = COPYING
 NODM_DEPENDENCIES = xlib_libX11 linux-pam
+NODM_AUTORECONF = YES
 
 $(eval $(autotools-package))
