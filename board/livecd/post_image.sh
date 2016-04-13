338a6abdbee0ecf565a811267f67e95f48e482cc
Matt Moretti <werebus@gmail.com>
2016-04-12 22:53:18 -0400

    Relative path to iso sym-link

--- board/livecd/post_image.sh
+++ board/livecd/post_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Virtualbox doesn't believe an .iso9660 file is an ISO
-ln -sf "${BINARIES_DIR}/rootfs.iso9660" "${BINARIES_DIR}/rootfs.iso"
+ln -sf "rootfs.iso9660" "${BINARIES_DIR}/rootfs.iso"
