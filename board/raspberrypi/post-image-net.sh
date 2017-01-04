51bfc57909b6ed0d499b0f06ce96b660794a5c1e
Matt Moretti <werebus@gmail.com>
2017-01-04 10:54:52 -0500

    Working RPi build

--- board/raspberrypi/post-image-net.sh
+++ board/raspberrypi/post-image-net.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GENIMAGE_CFG="${BR2_EXTERNAL}/board/raspberrypi/genimage.cfg"
+GENIMAGE_CFG="${BR2_EXTERNAL_BUS_INFO_BOARD_PATH}/board/raspberrypi/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 # Mark the kernel as DT-enabled
@@ -9,7 +9,7 @@ ${HOST_DIR}/usr/bin/mkknlimg "${BINARIES_DIR}/zImage" \
 	"${BINARIES_DIR}/kernel-marked/zImage"
 
 # Put our config.txt in place
-cp "${BR2_EXTERNAL}/board/raspberrypi/net-config.txt" "${BINARIES_DIR}/config.txt"
+cp "${BR2_EXTERNAL_BUS_INFO_BOARD_PATH}/board/raspberrypi/net-config.txt" "${BINARIES_DIR}/config.txt"
 
 rm -rf "${GENIMAGE_TMP}"
 
