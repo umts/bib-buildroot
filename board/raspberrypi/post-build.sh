c3b51130fd73d15db416a45321212581dfbd754c
Matt Moretti <werebus@gmail.com>
2017-01-19 16:19:54 -0500

    Whoops, actually look in the right file
    Turns out not to matter too much since the filesystem is created fresh
    with a fresh, full build, but if you rebuild, best to not keep adding
    onto /etc/fstab

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p $TARGET_DIR/boot
-grep -q 'mmcblk0p1' $TARGET_DIR/fstab || echo '/dev/mmcblk0p1	/boot		vfat	ro,noexec	0	0' >> $TARGET_DIR/etc/fstab
+grep -q 'mmcblk0p1' $TARGET_DIR/etc/fstab || echo '/dev/mmcblk0p1	/boot		vfat	ro,noexec	0	0' >> $TARGET_DIR/etc/fstab
